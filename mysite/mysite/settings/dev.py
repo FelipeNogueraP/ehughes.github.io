9d8f67e5a620e8e3374363152042e9f7256c8a1b
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2024-01-14 20:50:15 -0500

    Add wagtail to the project

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-tgv1n6m4v-24k-fkk3c6u1*=!p0qlnt+qpd3b!2%!6zf%u6j#n"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
